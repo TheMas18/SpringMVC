1600937d4481f3bc38ff12bfa77b669c4c1399fa
TheMas18 <mayureshsamant05@gmail.com>
2024-02-01 14:09:30 +0530

    Sending data controller to view using model

--- src/main/java/com/mvc/controller/HomeController.java
+++ src/main/java/com/mvc/controller/HomeController.java
@@ -1,22 +1,42 @@
 package com.mvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+//	@RequestMapping("/home")
+//	public String home() {
+//
+//		System.out.println("This is home url");
+//		return "index";
+//	}
+//	
+//	@RequestMapping("/about")
+//	public String about() {
+//
+//		System.out.println("This is about url");
+//		return "about";
+//	}
+	
+	//sending data from controller to view
+	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 
 		System.out.println("This is home url");
+		model.addAttribute("name","Mayuresh Samant");
+		List<String> friends=new ArrayList<>();
+		friends.add("Manoj");
+		friends.add("Pratham");
+		friends.add("Neha");
+		
+		model.addAttribute("f",friends);
 		return "index";
-	}
-	
-	@RequestMapping("/about")
-	public String about() {
-
-		System.out.println("This is about url");
-		return "about";
 	}
 
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -9,7 +9,29 @@
 <title>Home</title>
 </head>
 <body>
-	<h1>Hello Mas</h1>
+	<!-- 	<h1>Hello Mas</h1> -->
 	<h1>This is Home Page</h1>
+	<%
+	String name = (String) request.getAttribute("name");
+	List<String> frnds = (List<String>) request.getAttribute("f");
+	%>
+
+	<h1>
+		Hello
+		<%=name%></h1>
+	<h3>
+		<%
+		for (String s : frnds) {
+		%>
+		<h3>
+			<%
+			out.println(s);
+			%><br>
+		</h3>
+		<%
+		}
+		%>
+
+	</h3>
 </body>
 </html>
